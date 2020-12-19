32233c30cfcb2397aa106339515e52d989d658af
BackRunner <dev@backrunner.top>
2020-12-19 23:44:44 +0800

    fix: focus issue with drawer

--- packages/drawer/src/main.vue
+++ packages/drawer/src/main.vue
@@ -5,14 +5,13 @@
     @after-leave="afterLeave">
     <div
       class="el-drawer__wrapper"
-      tabindex="-1"
       v-show="visible">
       <div
         class="el-drawer__container"
         :class="visible && 'el-drawer__open'"
         @click.self="handleWrapperClick"
         role="document"
-        tabindex="-1">
+        >
         <div
           aria-modal="true"
           aria-labelledby="el-drawer__title"
@@ -22,11 +21,10 @@
           :style="isHorizontal ? `width: ${size}` : `height: ${size}`"
           ref="drawer"
           role="dialog"
-          tabindex="-1"
           >
           <header class="el-drawer__header" id="el-drawer__title" v-if="withHeader">
             <slot name="title">
-              <span role="heading" tabindex="0" :title="title">{{ title }}</span>
+              <span role="heading" :title="title">{{ title }}</span>
             </slot>
             <button
               :aria-label="`close ${title || 'drawer'}`"
@@ -37,7 +35,7 @@
               <i class="el-dialog__close el-icon el-icon-close"></i>
             </button>
           </header>
-          <section class="el-drawer__body" v-if="rendered">
+          <section class="el-drawer__body" ref="body" v-if="rendered">
             <slot></slot>
           </section>
         </div>
@@ -134,7 +132,7 @@ export default {
         }
         this.prevActiveElement = document.activeElement;
         this.$nextTick(() => {
-          Utils.focusFirstDescendant(this.$refs.drawer);
+          Utils.focusFirstDescendant(this.$refs.body);
         });
       } else {
         if (!this.closed) this.$emit('close');
