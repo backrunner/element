332cb7ded351aa074496a94b6a37ca3226407410
BackRunner <dev@backrunner.top>
2021-06-17 00:27:10 +0800

    build: 2.15.3-prev-1

--- src/index.js
+++ src/index.js
@@ -82,6 +82,8 @@ import CascaderPanel from '../packages/cascader-panel/index.js';
 import Avatar from '../packages/avatar/index.js';
 import Drawer from '../packages/drawer/index.js';
 import Popconfirm from '../packages/popconfirm/index.js';
+import Skeleton from '../packages/skeleton/index.js';
+import SkeletonItem from '../packages/skeleton-item/index.js';
 import locale from 'element-ui/src/locale';
 import CollapseTransition from 'element-ui/src/transitions/collapse-transition';
 
@@ -163,6 +165,8 @@ const components = [
   Avatar,
   Drawer,
   Popconfirm,
+  Skeleton,
+  SkeletonItem,
   CollapseTransition
 ];
 
@@ -198,7 +202,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '2.15.2-prev-1',
+  version: '2.15.3-prev-1',
   locale: locale.use,
   i18n: locale.i18n,
   install,
@@ -284,5 +288,7 @@ export default {
   CascaderPanel,
   Avatar,
   Drawer,
-  Popconfirm
+  Popconfirm,
+  Skeleton,
+  SkeletonItem
 };
